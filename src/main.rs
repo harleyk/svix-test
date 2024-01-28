068c3c2bbcaeb4d927367bec515aae981d1fe7eb
Harley Klein <harleyk@harleyklein.com>
2024-01-28 13:25:49 -0800

    feat: Initial Hello World project in Axum
    Based upon some examples in the tokio-rs/axum repository

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use std::time::Duration;
+
+use axum::response::IntoResponse;
+use axum::routing::get;
+use axum::Router;
+use tokio::net::TcpListener;
+use tower_http::timeout::TimeoutLayer;
+use tower_http::trace::TraceLayer;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+#[axum::debug_handler]
+async fn hello() -> impl IntoResponse {
+    "Hello world"
+}
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "svix-test=debug,tower_http=debug".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    let app = Router::new().route("/", get(hello)).layer((
+        TraceLayer::new_for_http(),
+        TimeoutLayer::new(Duration::from_secs(10)),
+    ));
+
+    let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
